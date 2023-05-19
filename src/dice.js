2369d57cb5757db5cb6e4f8dd895f6ff775bf4a4
Vigneron Tristan <arkalithe@gmail.com>
2023-05-19 10:59:17 +0200

    First draft Css

--- src/dice.js
+++ src/dice.js
@@ -1,6 +1,26 @@
+// let playerOneScore = "";
+// let playerTwoScore = "";
+
+// let letPlayere1Temp = "";
+// let letPlayere2Temp = "";
+
+
 // Nombre aléatoire de 1 a 6.
-const randomNumber = Math.floor(Math.random() * 6) + 1;
+var randomNumber = Math.floor(Math.random() * 6) + 1;
+var diceImages = '../images/dice' + randomNumber + '.png';
+const test = document.querySelector('img').setAttribute('src', diceImages);
 
-const diceImages = '../images/dice' + randomNumber + '.png';
+function rollTheDice() {
+    setTimeout( () => {
+        randomNumber;
+        diceImages;
+        test;
 
-document.querySelectorAll('img')[0].setAttribute('src', diceImages)
+    }, 2500)
+}
+
+
+
+
+
+
