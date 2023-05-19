5249789656e5884c070b55d1223e3737dc9fa1df
Vigneron Tristan <arkalithe@gmail.com>
2023-05-19 11:35:34 +0200

    Dom const / Param de base / Change Joueure / Lancé de dés

--- src/dice.js
+++ src/dice.js
@@ -1,23 +1,49 @@
-// let playerOneScore = "";
-// let playerTwoScore = "";
+// DOM const (A finir si besoin)
+const playerOne = document.querySelector(".player1")
+const playerTwo = document.querySelector(".player2") 
+const playerOneScore = document.getElementById("score1");
+const playerTwoScore = document.getElementById("score2");
+const playerOneCurrent = document.getElementById("current1");
+const playerTwoCurrent = document.getElementById("current2");
+const dice = document.querySelector('.dice');
+const btnNew = document.querySelector('.btnNew');
+const btnRoll =document.querySelector('.btnRoll');
+const btnHold =document.querySelector('.btnHold');
 
-// let letPlayere1Temp = "";
-// let letPlayere2Temp = "";
+// Paramètre de base
+playerOneScore.textContent = 0;
+playerTwoScore.textContent = 0;
+dice.classList.add("hidden");
+let currentScore = 0;
+const scores = [0,0];
+let activePlayer = 1;
+let playing = true;
 
+// Changer de joueure
+const changePlayer = function () {
+    currentScore = 0;
+    document.getElementById(`current${activePlayer}`).textContent = currentScore;
+    activePlayer = activePlayer == 1 ? 2 : 1;
+    playerOne.classList.toggle('activePlayer');
+    playerTwo.classList.toggle('activePlayer');
+};
 
-// Nombre aléatoire de 1 a 6.
-var randomNumber = Math.floor(Math.random() * 6) + 1;
-var diceImages = '../images/dice' + randomNumber + '.png';
-const test = document.querySelector('img').setAttribute('src', diceImages);
+// Lancer de dé
+btnRoll.addEventListener('click', function() {
+    if (playing) {
+        dice.classList.remove("hidden");
+        const diceResult = Math.floor(Math.random() * 6) + 1;
+        console.log(diceResult);
+        dice.src = `/images/dice${diceResult}.png`;
+        if (diceResult !== 1 ) {
+            currentScore += diceResult;
+            document.getElementById(`current${activePlayer}`).textContent = currentScore;
+        } else {
+            changePlayer();
+        }
+    }
+});
 
-function rollTheDice() {
-    setTimeout( () => {
-        randomNumber;
-        diceImages;
-        test;
-
-    }, 2500)
-}
 
 
 
